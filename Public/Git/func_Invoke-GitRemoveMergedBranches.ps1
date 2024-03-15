d6b201928056d21a9ab452c3df7d2512e35b10f5
Niels Nijholt <niels@nielsnijholt.nl>
2024-03-15 11:08:50 +0100

    Changed grmb so that it removes local branches that have been merged into the current branch, and it also prunes remote branches.

--- Public/Git/func_Invoke-GitRemoveMergedBranches.ps1
+++ Public/Git/func_Invoke-GitRemoveMergedBranches.ps1
@@ -5,20 +5,27 @@
  .Description
   Long format description of the function.
 
- #.Parameter DefaultParam
- Description of the DefaultParam parameter
-
  #.Example
  #  # Show default output of this command.
  #  Invoke-GitRemoveMergedBranches
 #>
 function Invoke-GitRemoveMergedBranches {
     [CmdletBinding()]
-    param(
-        [Parameter(Position = 0, Mandatory = $true)][String] $defaultParam
-    )
+    param()
 
-    Write-Output 'Default output of Invoke-GitRemoveMergedBranches.'
+    $currentBranch = git branch --show-current
+
+    # 1. Get all branches merged into the current branch
+    # 2. Select all that aren't the current branch
+    # 3. Delete selected branches
+    git branch --merged | Select-String -CaseSensitive -NotMatch -Pattern $currentBranch | ForEach-Object { git branch -D $_.ToString().Trim() }
+
+    # Prune remote branches
+    git fetch --prune
+
+    # Output branches
+    Write-Host "Remaining branches:"
+    git branch | ForEach-Object { Write-Host $_ }
 }
 
 New-Alias -Name ngrmb -Value Invoke-GitRemoveMergedBranches
